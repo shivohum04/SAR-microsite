f87d8514c6dc3251b1a6bab6b0c32a1516a2ca45
shivohum04 <shivohum04@gmail.com>
2024-01-26 20:26:48 +0530

    initialized vite (react) after server development

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 import { PORT,mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import { user } from './models/user_model.js';
@@ -10,7 +10,16 @@ app.get('/',(request,response)=>{
     return response.status(234).send('welcome to the microsite');
 });
 
+// middleware for parsing the request body
 app.use(express.json());
+
+// middleware to handle CORS policy 
+app.use(cors());
+app.get('/',(request,response)=>{
+    console.log(request);
+    return response.status(234).send("welcome to the microsite ");
+});
+
 app.use('/user',user_routes);
 
 mongoose
